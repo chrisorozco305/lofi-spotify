ec96c0c6fb5ea50b805cc74e00afd36f1159419c
Christian Orozco <chrisorozco305@gmail.com>
2025-06-02 18:07:05 -0400

    third push

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+  return (
+    <nav>
+      <ul className="flex">
+        <li><a href="/">Lofi Planet</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
